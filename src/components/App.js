0552be58171045c4664a000ce7aa137e8050e610
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2019-06-30 17:53:25 +0900

    add input and button

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,61 @@ import React, {Component} from 'react'
 import './App.css'
 
 export default class App extends Component {
+  state = {type: null, target: null}
+
+  _genrate = () => {
+    
+  }
+
+  _setType = (e) => {
+    console.log(e.target.value)
+    this.setState({type: e.target.value})
+  }
+  
+  _setTarget = (e) => {
+    console.log(e.target.value)
+    this.setState({target: e.target.value})
+  }
 
   render () {
     return (
       <div className='app'>
-        <h4>Auto Code Generator</h4>
-        <input type='text' />
+        <div>
+          <h3 className='h-title'>Auto Code Generator</h3>
+        </div>
+        <div>
+          <input className='input-target' type='text' onChange={this._setTarget} />
+        </div>
+        <div className='div-type'>
+          <input 
+            className='radio-type' 
+            type='radio' 
+            name='type' 
+            value='underbar'
+            onClick={this._setType}
+            defaultChecked
+          />underbar&nbsp;&nbsp;
+          <input 
+            className='radio-type' 
+            type='radio' 
+            name='type' 
+            value='hyphen' 
+            onClick={this._setType}
+          />hyphen&nbsp;&nbsp;
+          <input 
+            className='radio-type' 
+            type='radio' 
+            name='type' 
+            value='camelCase'
+            onClick={this._setType}
+          />camelCase
+        </div>
+        <div>
+          <button className='button-generate'>generate</button>
+        </div>
+        <div>
+          <p className='p-code'></p>
+        </div>
       </div>
     )
   }
