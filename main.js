0ecdc2658565f5ae1d80c33a0c9c6658eef5184c
Ian J.Um <31336310+code0xff@users.noreply.github.com>
2019-07-02 01:35:47 +0900

    add generate action

--- main.js
+++ main.js
@@ -12,7 +12,6 @@ function createWindow() {
 	})
 
 	win.loadFile('./dist/index.html')
-	win.webContents.openDevTools()
 
 	win.on('closed', () => {
 		win = null
--- src/components/App.js
+++ src/components/App.js
@@ -1,24 +1,49 @@
 import React, {Component} from 'react'
+import axios from 'axios'
 import './App.css'
+import properties from '../constants/properties.json'
 
 export default class App extends Component {
-  state = {type: null, target: null}
+  state = {type: 'underbar', target: null, code: '코드로 변환하실 내용을 입력해주세요.'}
 
-  _genrate = () => {
-    
+  _generate = () => {
+    axios.get(`http:/${properties.API_IP}:${properties.API_PORT}/translate?target=${this.state.target}`)
+      .then(response => {
+        const rawData = response.data.message.result.translatedText.toLowerCase()
+        const filteredData = rawData.replace(/[^a-zA-Z0-9 ]/gi, '')
+        const strings = filteredData.split(' ')
+        
+        let code = ''
+        const {type} = this.state
+        
+        if (type === 'underbar') {
+          code = strings.join('_').toUpperCase();
+        } else if (type === 'hyphen') {
+          code = strings.join('-').toLowerCase();
+        } else if (type === 'camelCase') {
+          code += strings[0]
+          for (let i = 1; i < strings.length; i++) {
+            code += (strings[i].charAt(0).toUpperCase() + strings[i].slice(1))
+          }
+        }
+        this.setState({code})
+      })
+      .catch(error => {
+        console.log(error);
+      })
   }
 
   _setType = (e) => {
-    console.log(e.target.value)
     this.setState({type: e.target.value})
   }
   
   _setTarget = (e) => {
-    console.log(e.target.value)
     this.setState({target: e.target.value})
   }
 
   render () {
+    let {code} = this.state
+
     return (
       <div className='app'>
         <div>
@@ -40,22 +65,22 @@ export default class App extends Component {
             className='radio-type' 
             type='radio' 
             name='type' 
-            value='hyphen' 
+            value='camelCase'
             onClick={this._setType}
-          />hyphen&nbsp;&nbsp;
+          />camelCase&nbsp;&nbsp;
           <input 
             className='radio-type' 
             type='radio' 
             name='type' 
-            value='camelCase'
+            value='hyphen' 
             onClick={this._setType}
-          />camelCase
+          />hyphen
         </div>
         <div>
-          <button className='button-generate'>generate</button>
+          <button className='button-generate' onClick={this._generate}>generate</button>
         </div>
         <div>
-          <p className='p-code'></p>
+          <p className='p-code'>{code}</p>
         </div>
       </div>
     )
